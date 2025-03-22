52d5b17a2dabc1b1fd1ff02cf327b1453ac5c9d0
safety-crew <153456442+safety-crew@users.noreply.github.com>
2025-03-22 20:37:45 +0200

    Create NavItem.jsx and Add Navbar Items to Header

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <>
       <Header />
-      <Container />
+      <Container>Content</Container>
       <Footer />
     </>
   );
--- src/Components/Container/Container.jsx
+++ src/Components/Container/Container.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Container.css";
 
-const Container = () => {
-  return <div>Container</div>;
+const Container = (props) => {
+  return <div>{props.children}</div>;
 };
 
 export default Container;
