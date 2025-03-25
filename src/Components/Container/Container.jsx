b5264d1a277aceb80ef891a1061e1fb99b713806
safety-crew <153456442+safety-crew@users.noreply.github.com>
2025-03-25 14:27:30 +0200

    Build the Container

--- src/Components/Container/Container.jsx
+++ src/Components/Container/Container.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Container.css";
 
 const Container = (props) => {
-  return <div>{props.children}</div>;
+  return <div className="container main-container">{props.children}</div>;
 };
 
 export default Container;
