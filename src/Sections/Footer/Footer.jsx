fbc7addf239bf67ae364a0ac35dc250ad32a71da
safety-crew <153456442+safety-crew@users.noreply.github.com>
2025-03-27 16:29:32 +0200

    Add Footer Section

--- src/Sections/Footer/Footer.jsx
+++ src/Sections/Footer/Footer.jsx
@@ -2,7 +2,29 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <p>
+              Copyright © 2036 <a href="#">Cyborg Gaming</a> Company. All rights
+              reserved.
+              <br />
+              Design:{" "}
+              <a
+                href="https://templatemo.com"
+                target="_blank"
+                title="free CSS templates"
+              >
+                TemplateMo
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
